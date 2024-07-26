45ed09e790edf0a2f07554b9d3f28f37250f6d30
Denis LAvrov <42438187+DenisLAvrov14@users.noreply.github.com>
2024-07-27 01:23:01 +0400

    Update database connection configuration for SSL

--- db.js
+++ db.js
@@ -2,13 +2,8 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    require: true,
     rejectUnauthorized: false
   }
 });
