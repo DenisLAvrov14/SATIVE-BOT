f59c7d513f584549a2717e23b728386f7ddcfea7
Denis LAvrov <42438187+DenisLAvrov14@users.noreply.github.com>
2024-07-26 13:42:51 +0400

    change db.js

--- db.js
+++ db.js
@@ -18,4 +18,16 @@ const client = new Client({
     client.end();
   });
 
+async function loadBookings() {
+  const client = await pool.connect();
+  try {
+    const result = await client.query('SELECT * FROM bookings WHERE booking_date >= CURRENT_DATE');
+    return result.rows;
+  } catch (err) {
+    console.error('Error loading bookings:', err);
+  } finally {
+    client.release();
+  }
+}
+
 module.exports = { loadBookings };
