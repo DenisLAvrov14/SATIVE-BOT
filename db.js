7e5a8662d921951174be8613b44a7f984ac64511
Denis LAvrov <42438187+DenisLAvrov14@users.noreply.github.com>
2024-07-26 14:43:28 +0400

    change db.js

--- db.js
+++ db.js
@@ -2,31 +2,38 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
+
+async function loadBookings() {
+  const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false
     }
   });
-  
-  client.connect();
-  
-  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
 
-async function loadBookings() {
-  const client = await pool.connect();
+  await client.connect();
   try {
     const result = await client.query('SELECT * FROM bookings WHERE booking_date >= CURRENT_DATE');
     return result.rows;
   } catch (err) {
     console.error('Error loading bookings:', err);
   } finally {
-    client.release();
+    await client.end();
   }
 }
 
